e562a3974d774c0374dfa4883295c5d9ac9e6f85
dbaproject <dba.project@gmail.com>
2015-09-03 11:14:27 +0900

    test

--- js/sample.js
+++ js/sample.js
@@ -1,48 +1,58 @@
-
-// 데이터셋이 CSV 파일일 때(헤더가 한글일 때)
+// 데이터를 저장할 배열 준비
+var dataSet = [ ];
+// CSV 파일을 불러와 그래프 그리기
 d3.tsv("https://raw.githubusercontent.com/dbaproject/d3.js/master/src/mydata.tsv", function(error, data){
-	var dataSet = [];	// 데이터를 저장할 배열을 준비
-	for(var i=0; i < data.length; i++){	// 데이터 줄 수만큼 반복
-		dataSet.push(data[i].item1);	// 상품A의 레이블 데이터만 추출
+	for(var i=0; i<data.length; i++){	// 데이터 줄 수만큼 반복
+		dataSet.push(data[i].item1);	// item1 레이블의 데이터만 추출
 	}
-	// 그래프 그리기
+	// 데이터를 이용하여 그래프 그리기
+	d3.select("#myGraph")	// SVG 요소 지정
+		.selectAll("rect")	// SVG에서 사각형을 나타낼 요소를 지정
+		.data(dataSet)	// 데이터 설정
+		.enter()	// 데이터 개수에 따라 Rect 요소 생성
+		.append("rect")	// SVG 사각형 생성
+		.attr("x", 10)	// 가로형 막대그래프이므로 X 좌표를 10으로 조정
+		.attr("y", function(d,i){	// Y 좌표를 배열 순서에 따라 계산
+			return i * 25;	// 막대그래프의 Y 좌표를 25px 단위로 계산
+		})
+		.on("click", function(){
+			d3.select(this)	// 클릭한 요소를 지정
+				.style("fill", "cyan")	// 채우기 스타일을 하늘색으로 지정
+		})
+		.attr("height", "20px")	// 막대그래프의 높이를 20px로 지정
+		.attr("width", "0px")	// 처음에는 막대그래프의 넓이를 0px로 지정
+		.transition()	// 그래프에 애니메이션 적용
+		.delay(function(d, i){
+			return i * 500;	// 0.5초마다 그리도록 대기 시간을 설정
+		})
+		.duration(2500)	// 2.5초 동안 애니메이션이 진행되도록 함
+		.attr("width", function(d, i){	// 넓이를 배열의 내용에 따라 계산
+			return d +"px";	// 데이터의 값을 그대로 넓이로 함
+		})
+	// 눈금을 표시하기 위한 선형 스케일 설정
+	var xScale = d3.scale.linear()  // 선형 스케일 설정
+		.domain([0, 300])   // 원래 데이터 범위
+		.range([0, 300]);  // 실제 출력 크기
+	// 눈금을 설정하고 표시함
 	d3.select("#myGraph")
-	  .selectAll("rect")	// rect 요소 지정
-	  .data(dataSet)	// 데이터를 요소에 연결
-	  .enter()	// 데이터 개수만큼 반복
-	  .append("rect")	// 데이터 개수만큼 rect 요소가 추가됨
-	  .attr("class", "bar")	// CSS 클래스를 지정
-	  .attr("width", "0px")				// 최초 막대그래프의 넓이를 0px로 지정
-	  .transition()							// 그래프 출력 시 애니메이션 효과 적용
-	  .delay(function(d, i){
-		return i * 500;					// 0.5초마다 그리도록 대기 시간을 설정
-	       })
-	  .duration(2500)					// 2.5초에 걸쳐 애니메이션화 함
-	  .attr("width", function(d,i){	// 넓이를 지정. 두 번째의 파라미터에 함수를 지정
-			return d;	// 데이터 값을 그대로 넓이로 반환
+		.append("g")	// 그룹화함
+		.attr("class", "axis")	// 스타일시트 클래스를 설정
+		.attr("transform", "translate(10, "+((1+dataSet.length) * 20+5)+")")	// 표시 위치 조정
+		.call(d3.svg.axis()	// call()로 눈금을 표시할 함수를 호출
+			.scale(xScale)  // 스케일을 적용
+			.orient("bottom")   // 눈금의 표시 위치를 아래쪽으로 지정
+		)
+});
+// 버튼을 클릭했을 때의 처리
+d3.select("#updateButton").on("click", function(){
+	for(var i=0; i<dataSet.length; i++){
+		dataSet[i] = Math.floor(Math.random() * 320);	// 0〜320미만의 값을 생성
+	}
+	d3.select("#myGraph")	// SVG 요소 지정
+		.selectAll("rect")	// SVG에서 사각형을 나타낼 요소를 지정
+		.data(dataSet)	// 데이터 설정
+		.transition()	// 변환 표시
+		.attr("width", function(d, i){	// 넓이를 배열의 내용에 따라 계산
+			return d +"px";	// 데이터의 값을 그대로 넓이로 함
 		})
-	  .attr("height", 20)	// 높이를 지정
-	  .attr("x", 0)	// X 좌표를 0으로 함
-	  .attr("y", function(d, i){	// Y 좌표를 지정함
-			return i * 25	 ; //표시 순서에 25를 곱해 위치를 계산
-		})
-  
-      .on("click",function(){
-       d3.select(this)
-         .style("fill","cyan")
-    })
-  
-    d3.select("#updateButton")
-      .on("click",function(){
-      for(var i=0;i<dataSet.length;i++) {
-        dataSet[i] = Math.floor(Math.random() * 320);
-      }
-    d3.select("#myGraph")
-      .selectAll("rect")
-      .data(dataSet)
-      .transition()
-      .attr("width",function(d,i){
-      return d + "px";
-    })
-})
 })
